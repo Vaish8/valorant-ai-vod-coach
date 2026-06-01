597d706231ee2006f765f7b2a66bfb8989ce6263
Vaish8 <vmalvankar18@gmail.com>
2026-06-01 14:23:43 +0100

    fix: export database models

---
+++ backend/app/db/models/__init__.py
@@ -0,0 +1,6 @@
+from app.db.models.analysis_finding import AnalysisFinding
+from app.db.models.event import Event
+from app.db.models.match import Match
+from app.db.models.round import Round
+
+__all__ = ["Match", "Round", "Event", "AnalysisFinding"]
